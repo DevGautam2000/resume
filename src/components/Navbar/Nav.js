e5e287790defec160549dec03de5663bcde33fa5
Gautam Chandra Saha <devgautam1231@gmail.com>
2022-02-09 20:11:53 +0530

    updated resume

--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Nav.css";
-import Pdf from "../../assets/resume_new.pdf";
+import Pdf from "../../assets/resume.pdf";
 
 function Nav({ bg, text }) {
   const onResumeClick = () => {
